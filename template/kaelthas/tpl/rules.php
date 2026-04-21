10d766e65bf8113f19b98d541476c44bfe1e1832
preeminence87 <the_nate_vasquez@hotmail.com>
2026-04-20 22:01:25 -0700

    Modified setup instructions

--- template/kaelthas/tpl/rules.php
+++ template/kaelthas/tpl/rules.php
@@ -11,11 +11,10 @@
     <?php elang('read_before_register'); ?>
 </p>
 <ul>
-    <li><?php elang('rule'); ?> 1.</li>
-    <li><?php elang('rule'); ?> 2.</li>
-    <li><?php elang('rule'); ?> 3.</li>
-    <li><?php elang('rule'); ?> 4.</li>
-    <li><?php elang('rule'); ?> 5.</li>
-    <li><?php elang('rule'); ?> 6.</li>
-    <li><?php elang('edit_on'); ?> <b>"/template/<?php echo $antiXss->xss_clean(get_config("template")); ?>/tpl/rules.php"</b>.</li>
+    <li>1: Register your account on this page.</li>
+    <li>2: Download the game client from the 'Register' tab above.</li>
+    <li>3: Extract the game client to your user folder.</li>
+    <li>4: Locate the realmlist file and open with a text editor.</li>
+    <li>5: Change the hostname to what's displayed on the upper right.</li>
+    <li>6: Launch the game and login with the username (not email) and password you configure here.</li>
 </ul>
